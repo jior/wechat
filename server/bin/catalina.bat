490c0ced72997f97f05e758abac9aa58a41c261a
jior <jior2008@gmail.com>
2014-11-12 10:27:27 +0800

    remove jrebel

--- server/bin/catalina.bat
+++ server/bin/catalina.bat
@@ -176,7 +176,6 @@ set "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\bin\tomcat-juli.jar"
 
 
 set JAVA_OPTS=%JAVA_OPTS% -Xmx512m -XX:PermSize=128m -XX:MaxPermSize=256m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=7 -XX:+UseCMSInitiatingOccupancyOnly
-set JAVA_OPTS=%JAVA_OPTS%  -Drebel.packages_include=com.glaf -Drebel.aspectj_plugin=true -Drebel.spring_plugin=true  -Drebel.resteasy_plugin=true -Drebel.mybatis_plugin=true -Drebel.jackson_plugin=true -noverify -javaagent:./jrebel.jar
 
 if not "%LOGGING_CONFIG%" == "" goto noJuliConfig
 set LOGGING_CONFIG=-Dnop
